e6010c45b961171def5174467e1b8f767e2b6ed1
unknown <martin_suarez_mendez@outlook.com>
2021-05-03 15:35:22 -0500

    seeders part 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Course;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $course2 = new Course();
+
+        $course2->name = "name2";
+        $course2->description = "description2";
+        $course2->category = "category3";
+        $course2->save();
     }
 }
