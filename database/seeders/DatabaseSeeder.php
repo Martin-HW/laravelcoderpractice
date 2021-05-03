b5a9b888674e8502236fa68b272579c8337a99b6
unknown <martin_suarez_mendez@outlook.com>
2021-05-03 16:04:35 -0500

    factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Course;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(50)->create();
+        Course::factory(50)->create();
+
         // $course2 = new Course();
 
         // $course2->name = "name2";
         // $course2->description = "description2";
         // $course2->category = "category3";
         // $course2->save();
-        $this->call(CourseSeeder::class);
+        // $this->call(CourseSeeder::class);
     }
 }
